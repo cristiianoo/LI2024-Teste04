c205388245c918d4afa752d984e65a8d17e7d5dc
Cristiano <cristianosaraiva1010@gmail.com>
2024-12-02 10:38:08 +0000

    5ª tarefa concluída

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-//ficheiro de script
+// Script para validação customizada do Bootstrap
+(function () {
+    'use strict';
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.from(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
